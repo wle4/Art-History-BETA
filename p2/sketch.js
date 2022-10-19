07ed8d5a43992e93d2ec213eadb87793bad7cec5
wle4 <114623158+wle4@users.noreply.github.com>
2022-10-19 18:40:05 -0500

    Add files via upload

---
+++ p2/sketch.js
@@ -0,0 +1,30 @@
+let num;
+let img;
+function preload() {
+  img = loadImage('statu2.jpg');
+}
+
+function setup() {
+  let canvas = createCanvas(0.5*windowWidth, 0.5*windowWidth); //create canvas half screen width
+  canvas.addClass('centered'); //add centered class to canvas (see style.css)
+   //get color value from URL
+  let params = getParams();
+  let clr = params.color; 
+  num = params.num; 
+  //add color values in link to next page
+  var a = document.getElementsByTagName('a')[0];
+  var href = setParams(a.href, 'color', clr);
+  a.href = href;
+
+  image(img, 10, 10, 50, 50);
+
+  background(clr);
+  fill('yellow');
+  noStroke();
+}
+
+function draw() {
+  for(var i = 0; i < num; i++){
+    ellipse(width/2, 50+i*30, 20);
+  }
+}
