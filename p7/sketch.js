fd8aecb2601ac3f729ee57f01d2c7d9e017d3cbf
wle4 <114623158+wle4@users.noreply.github.com>
2022-11-09 15:45:50 -0600

    Update sketch.js

--- p7/sketch.js
+++ p7/sketch.js
@@ -35,7 +35,7 @@ function draw() {
   {if (bedX > 220 && bedY > 190 && bedX < 270 && bedY < 240)
     fill(240,248,255)
   {if (bedX > 220 && bedY > 190 && bedX < 270 && bedY < 240)
-    text(s, 200, 40, 350, 600); 
+    text(s, 200, 300, 350, 600); 
     textFont('Georgia');
     textSize(15)
     textStyle(BOLD);
