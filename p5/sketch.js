6311102216be12c8c16e98daf4d01b73e72ff012
wle4 <114623158+wle4@users.noreply.github.com>
2022-11-09 15:25:15 -0600

    Update sketch.js

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,17 +1,17 @@
 var bedX = 0;
 var bedY = 0;
-let s = 'Located in the southern part of Argentina, these paintings were created from 9,000-13,000 years ago. Natural paint was used from iron and various other minerals.';
+let s = 'Raphael, a famous Renaissance painter, demonstrates skill in this painting. During the Middle Ages, perspective was often not as defined as the Renaissance. The Renaissance era proved they could develop more complex art skills, and developed new techniques using visual rays to make images seem close/far';
 
 function preload() {
-  img = loadImage("10.png");
-  base = loadImage("caveman..jpg")
+  img = loadImage("13.png");
+  base = loadImage("renaissance.jpg")
 }
 
 function mouseDragged() {
   if ((mouseX > bedX - 200) && (mouseX < bedX + 200)) {
   if ((mouseY > bedY - 200) && (mouseY < bedY + 200)) {
-      bedX = mouseX-80;
-      bedY = mouseY-80
+      bedX = mouseX-40;
+      bedY = mouseY-40
     }
   }
 }
@@ -21,21 +21,23 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background(190);
 
-  image(img, bedX, bedY, 100, 100);
-  rect(300, 200, 100, 100)
+  image(img, bedX, bedY, 150, 150);
+  rect(550, 130, 100, 100)
   fill(100); 
   fill(100,50); 
   fill(0,0,255);
   fill(0,0,255,50);
   
-  {if (bedX > 250 && bedY > 150 && bedX < 350 && bedY < 250)
-    background(base); }
-   {if (bedX > 250 && bedY > 150 && bedX < 350 && bedY < 250)
+  {if (bedX > 550 && bedY > 130 && bedX < 600 && bedY < 180)
+    background(0);}
+  {if (bedX > 550 && bedY > 130 && bedX < 600 && bedY < 180)
+    image(base,windowWidth/4, 0, windowWidth/2, windowHeight) }
+  {if (bedX > 550 && bedY > 130 && bedX < 600 && bedY < 180)
     fill(240,248,255)
-   {if (bedX > 250 && bedY > 150 && bedX < 350 && bedY < 250)
-    text(s, windowWidth/5, windowHeight/1.5, 350, 500); 
+  {if (bedX > 550 && bedY > 130 && bedX < 600 && bedY < 180)
+    text(s, 20, 40, 350, 600); 
     textFont('Georgia');
     textSize(15)
     textStyle(BOLD);
@@ -46,3 +48,4 @@ function draw() {
 
   
 }
+//https://www.dailyartmagazine.com/the-marriage-of-the-virgin/
