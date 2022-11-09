1e572a9180cb2e19d82f92264433db1e8884148b
wle4 <114623158+wle4@users.noreply.github.com>
2022-11-09 01:55:54 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 var bedX = 0;
 var bedY = 0;
+let s = 'Van Gogh made this painting to represent his bedroom. Although a bedroom should make a person feel comfort, the contrasting colors almost feel anxious and restless. The flatness of the painting was with intent. He was inspired by Japanese prints in this painting';
 
 function preload() {
   img = loadImage("BED.png");
@@ -23,10 +24,25 @@ function draw() {
   background(220);
 
   image(img, bedX, bedY, 330, 330);
+  rect(windowWidth/2, windowHeight/3, 200, 200)
+  fill(100); 
+  fill(100,50); 
+  fill(0,0,255);
+  fill(0,0,255,50);
   
   {if (bedX > windowWidth/3 && bedY > windowHeight/5)
-      background(bedroom);
-      
-    }
+    background(bedroom); }
+  {if (bedX > windowWidth/3 && bedY > windowHeight/5)
+    fill(0)
+  {if (bedX > windowWidth/3 && bedY > windowHeight/5)
+    text(s, windowWidth/5, windowHeight/1.5, 350, 500); 
+    textFont('Georgia');
+    textSize(15)
+    textStyle(BOLD);
+  }
+
+   
+  }
+
   
 }
