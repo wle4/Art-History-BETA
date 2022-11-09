03a2f8cc0463efc6aac06a72ddebfa76f91f526d
wle4 <114623158+wle4@users.noreply.github.com>
2022-11-09 01:09:51 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,64 +1,32 @@
-let bx;
-let by;
-let boxSize = 150;
-let overBox = false;
-let locked = false;
-let xOffset = 0.0;
-let yOffset = 0.0;
-let bed;
+var bedX = 0;
+var bedY = 0;
+
 function preload() {
-  bed = loadImage('CHAIR1.png');
+  img = loadImage("BED.png");
+  bedroom = loadImage("bedroomm.jpg")
+}
+
+function mouseDragged() {
+  if ((mouseX > bedX - 200) && (mouseX < bedX + 200)) {
+  if ((mouseY > bedY - 200) && (mouseY < bedY + 200)) {
+      bedX = mouseX-50;
+      bedY = mouseY-50
+    }
+  }
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  bx = width / 5.0;
-  by = height / 5.0;
-}
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(500);
-if (
-    mouseX > bx - boxSize &&
-    mouseX < bx + boxSize &&
-    mouseY > by - boxSize &&
-    mouseY < by + boxSize
-  ) {
-    overBox = true;
-    if (!locked) {
+  background(220);
+
+  image(img, bedX, bedY, 330, 330);
+  
+  {if (bedX > windowWidth/3 && bedY > windowHeight/5)
+      background(bedroom);
+      
     }
-  } else {
-    overBox = false;
-  }
-
-  // Draw the box
-  image(bed, bx, by, boxSize, boxSize);
-}
-
-function mousePressed() {
-  if (overBox) {
-    locked = true;
-  } else {
-    locked = false;
-  }
-  xOffset = mouseX - bx;
-  yOffset = mouseY - by;
-}
-
-function mouseDragged() {
-  if (xOffset < 130)
-  if (xOffset > 20) 
-  if (yOffset > 20)
-  if (yOffset < 130){
-    bx = mouseX - xOffset;
-    by = mouseY - yOffset;
-  }
-}
-
-function mouseReleased() {
-  locked = false;
-
+  
 }
