349b9a7f15cef7072712435015868f8587a0b65a
wle4 <114623158+wle4@users.noreply.github.com>
2022-11-09 15:26:17 -0600

    Update sketch.js

--- p7/sketch.js
+++ p7/sketch.js
@@ -1,17 +1,17 @@
 var bedX = 0;
 var bedY = 0;
-let s = 'Located in the southern part of Argentina, these paintings were created from 9,000-13,000 years ago. Natural paint was used from iron and various other minerals.';
+let s = 'Warhol, a famous contemporary artist, made this to signify the mass-production of products. He made it to represent almost like a grocery store shelf, to demonstrate todays life of mass consumption and production';
 
 function preload() {
-  img = loadImage("10.png");
-  base = loadImage("caveman..jpg")
+  img = loadImage("16.png");
+  base = loadImage("warhol.jpg")
 }
 
 function mouseDragged() {
   if ((mouseX > bedX - 200) && (mouseX < bedX + 200)) {
   if ((mouseY > bedY - 200) && (mouseY < bedY + 200)) {
-      bedX = mouseX-80;
-      bedY = mouseY-80
+      bedX = mouseX-40;
+      bedY = mouseY-40
     }
   }
 }
@@ -21,21 +21,21 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background(190);
 
-  image(img, bedX, bedY, 100, 100);
-  rect(300, 200, 100, 100)
+  image(img, bedX, bedY, 150, 150);
+  rect(220, 190, 100, 100)
   fill(100); 
   fill(100,50); 
   fill(0,0,255);
   fill(0,0,255,50);
   
-  {if (bedX > 250 && bedY > 150 && bedX < 350 && bedY < 250)
-    background(base); }
-   {if (bedX > 250 && bedY > 150 && bedX < 350 && bedY < 250)
-    fill(240,248,255)
-   {if (bedX > 250 && bedY > 150 && bedX < 350 && bedY < 250)
-    text(s, windowWidth/5, windowHeight/1.5, 350, 500); 
+  {if (bedX > 220 && bedY > 190 && bedX < 270 && bedY < 240)
+    background(base);}
+  {if (bedX > 220 && bedY > 190 && bedX < 270 && bedY < 240)
+    fill(0)
+  {if (bedX > 220 && bedY > 190 && bedX < 270 && bedY < 240)
+    text(s, 20, 40, 350, 600); 
     textFont('Georgia');
     textSize(15)
     textStyle(BOLD);
@@ -46,3 +46,4 @@ function draw() {
 
   
 }
+//https://origins.osu.edu/milestones/november-2012-andy-warhol-s-campbell-s-soup-cans-1962?language_content_entity=en
